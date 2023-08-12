ae11dc38297447f26f123beea7625189215bf945
Yarden Shoham <git@yardenshoham.com>
2023-08-12 20:42:11 +0300

    Use dark theme

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,10 +1,20 @@
 import { Head } from "aleph/framework/react/head.ts";
-
+import {
+  createTheme,
+  ThemeProvider,
+} from "https://esm.sh/@mui/material@5.14.4/styles";
+import CssBaseline from "https://esm.sh/@mui/material@5.14.4/CssBaseline";
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 export default function App({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <Head></Head>
       {children}
-    </>
+    </ThemeProvider>
   );
 }
