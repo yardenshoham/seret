7b8de74d811a5cbb61abf1abdaa0e8e129be220d
ofirdassa9 <59168833+ofirdassa9@users.noreply.github.com>
2023-08-18 09:13:17 +0300

    Deno devcontainer (#22)
    Added Github Codespaces configuration

--- routes/_export.ts
+++ routes/_export.ts
@@ -1,14 +1,14 @@
 // Exports router modules for serverless env that doesn't support the dynamic import.
 // This module will be updated automaticlly in develoment mode, do NOT edit it manually.
 
-import * as $0 from "./_app.tsx";
-import * as $1 from "./_404.tsx";
+import * as $0 from "./_404.tsx";
+import * as $1 from "./_app.tsx";
 import * as $2 from "./index.tsx";
 import * as $3 from "./movie/$movie.tsx";
 
 export default {
-  "/_app": $0,
-  "/_404": $1,
+  "/_404": $0,
+  "/_app": $1,
   "/": $2,
   "/movie/:movie": $3,
 };
